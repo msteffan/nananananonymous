7b5f980d59f63ff1f268fb7304c3269c745904ac
MikeDulik <mikedulik@gmail.com>
2015-08-10 17:28:21 -0400

    controllers init

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -1,3 +1,40 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :create, :edit, :update, :destroy]
+  # before_action :authenticate_user!, only: [:show, :create, :edit, :update, :destroy]
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(params[:event])
+    if @event.save
+      redirect_to "/events/index"
+    else
+      redirect_to "/events/new"
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    @event.update(params[:event])
+    redirect_to "/events/index"
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to "/events/index"
+  end
 end
--- app/controllers/response_controller.rb
+++ app/controllers/response_controller.rb
@@ -1,3 +1,40 @@
 class ResponsesController < ApplicationController
-  before_action :authenticate_user!, only: [:show, :create, :edit, :update, :destroy]
+  # before_action :authenticate_user!, only: [:show, :create, :edit, :update, :destroy]
+
+  def index
+    @responses = Response.all
+  end
+
+  def show
+    @response = Response.find(params[:id])
+  end
+
+  def new
+    @response = Response.new
+  end
+
+  def create
+    @response = Response.new(params(:response))
+    if @response.save
+      redirect_to "/thankyou"
+    else
+      redirect_to "/responses/new"
+    end
+  end
+
+  def edit
+    @response = Response.find(params[:id])
+  end
+
+  def update
+    @response = Response.find(params[:id])
+    @response.update(params[:response])
+    # redirect?
+  end
+
+  def destroy
+    @response = Response.find(params[:id])
+    @response.destroy
+    redirect_to "/events/index"
+  end
 end
