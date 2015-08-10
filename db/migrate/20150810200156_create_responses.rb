89f71696fba81943e57ee89f9e20d44f6c2209ec
Paul Doulatshahi <paul.doulatshahi@gmail.com>
2015-08-10 16:09:33 -0400

    created migrations, routes, and added devise

---
+++ db/migrate/20150810200156_create_responses.rb
@@ -0,0 +1,11 @@
+class CreateResponses < ActiveRecord::Migration
+  def change
+    create_table :responses do |t|
+      t.integer :comfort_score
+      t.integer :completeness_score
+      t.timestamps
+      t.string :name
+      t.references :event, index: true, foreign_key: true
+    end
+  end
+end
