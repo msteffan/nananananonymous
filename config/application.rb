3650b64517e491f58d5bc297d7f633d3f1289437
Melissa Steffan <msteffan2012@gmail.com>
2015-08-14 00:01:52 -0400

    updated CSS for report page

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Nananananonymous
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
-    config.serve_static_assets = true
+    config.serve_static_files = true
     config.assets.initialize_on_precompile = true
   end
 end
